047e9f1fb8a9b89cf80dcf9e65e330686b35c683
Zachary Clark <zacclark369@gmail.com>
2021-12-04 13:56:11 -0800

    MVP

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -14,31 +14,31 @@ const questionsArray = [
     options: ["ad", "fa", "da", "dada"],
     correctAnswer: "dada",
   },
-  {
-    Question: "",
-    options: ["", "", "", ""],
-    correctAnswer: "",
-  },
-  {
-    Question: "",
-    options: ["", "", "", ""],
-    correctAnswer: "",
-  },
-  {
-    Question: "",
-    options: ["", "", "", ""],
-    correctAnswer: "",
-  },
-  {
-    Question: "",
-    options: ["", "", "", ""],
-    correctAnswer: "",
-  },
-  {
-    Question: "",
-    options: ["", "", "", ""],
-    correctAnswer: "",
-  },
+  //   {
+  //     Question: "",
+  //     options: ["", "", "", ""],
+  //     correctAnswer: "",
+  //   },
+  //   {
+  //     Question: "",
+  //     options: ["", "", "", ""],
+  //     correctAnswer: "",
+  //   },
+  //   {
+  //     Question: "",
+  //     options: ["", "", "", ""],
+  //     correctAnswer: "",
+  //   },
+  //   {
+  //     Question: "",
+  //     options: ["", "", "", ""],
+  //     correctAnswer: "",
+  //   },
+  //   {
+  //     Question: "",
+  //     options: ["", "", "", ""],
+  //     correctAnswer: "",
+  //   },
 ];
 
 // begin with timer of 1:30 min (90sec)
@@ -91,41 +91,41 @@ function beginStartButton() {
   startQuestions();
 }
 
-  function startQuestions() {
-    if (questionsArray[questionCount]) {
-      questionTitleEl.textContent = questionsArray[questionCount].Question;
-      questionEl.innerHTML = "";
-      for (let i = 0; i < questionsArray[questionCount].options.length; i++) {
-        let option = document.createElement("button");
-        option.setAttribute("class", "select-btn");
-        option.setAttribute("value", questionsArray[questionCount].options[i]);
-        option.textContent = questionsArray[questionCount].options[i];
-        questionEl.appendChild(option);
-        option.onclick = questionClick;
-      }
+function startQuestions() {
+  if (questionsArray[questionCount]) {
+    questionTitleEl.textContent = questionsArray[questionCount].Question;
+    questionEl.innerHTML = "";
+    for (let i = 0; i < questionsArray[questionCount].options.length; i++) {
+      let option = document.createElement("button");
+      option.setAttribute("class", "select-btn");
+      option.setAttribute("value", questionsArray[questionCount].options[i]);
+      option.textContent = questionsArray[questionCount].options[i];
+      questionEl.appendChild(option);
+      option.onclick = questionClick;
     }
   }
-  function questionClick() {
-    if (questionCount === questionsArray.length) {
-      endGame(timeRemaining);
-    }
+}
+function questionClick() {
+  if (questionCount === questionsArray.length) {
+    endGame(timeRemaining);
+  }
 
-    const usersChoice = this.value;
-    console.log(usersChoice);
-    const correctAnswer = questionsArray[questionCount].correctAnswer;
-    questionCount++;
-    if (usersChoice === correctAnswer) {
-      textColorEl.textContent = "Correct Answer! ";
-      textColorEl.style.color = "green";
-      announceEl.textcontent = "Next Question:";
-    } else {
-      timeRemaining -= 5;
-      textColorEl.textContent = "Wrong Answer! ";
-      textColorEl.style.color = "red";
-      announceEl.textcontent = "-5 seconds, please repeat the question: ";
-    }
-    startQuestions();
+  const usersChoice = this.value;
+  console.log(usersChoice);
+  const correctAnswer = questionsArray[questionCount].correctAnswer;
+  questionCount++;
+  if (usersChoice === correctAnswer) {
+    textColorEl.textContent = "Correct Answer! ";
+    textColorEl.style.color = "green";
+    announceEl.textcontent = "Next Question:";
+  } else {
+    timeRemaining -= 5;
+    textColorEl.textContent = "Wrong Answer! ";
+    textColorEl.style.color = "red";
+    announceEl.textcontent = "-5 seconds, please repeat the question: ";
   }
+  startQuestions();
+}
 
 //   console.log("endgame");
 function endGame(timeRemaining) {
@@ -143,114 +143,119 @@ function endGame(timeRemaining) {
   questionEl.remove();
 
   scoreForm();
-}
-function scoreForm() {
-  const inputEl = document.createElement("input");
-  inputEl.placeholder = "Please enter the name you would like saved";
-  inputEl.name = "name";
-  inputEl.id = "initials-input";
 
-  const saveEl = document.createElement("save");
-  saveEl.id = "submission";
-  saveEl.classList.add("btn-primary");
-  saveEl.textContent = "SAVE";
-  // insert into list form
-  containerEl.appendChild(inputEl);
-  containerEl.appendChild(saveEl);
+  function scoreForm() {
+    console.log("scoreForm");
+    const inputEl = document.createElement("INPUT");
+    inputEl.setAttribute("type", "text");
+    inputEl.placeholder = "Please enter the name you would like saved";
+    inputEl.name = "initials";
+    inputEl.id = "initials-input";
 
-  saveEl.addEventListener("save", handleScore);
-}
+    const saveEl = document.createElement("btn");
+    saveEl.id = "submission";
+    saveEl.classList.add("btn-primary");
+    saveEl.textContent = "SAVE";
+    // insert into list form
 
-// here, here, here, inspect these elements for typos
+    console.log(inputEl, saveEl);
+    containerEl.appendChild(inputEl);
+    containerEl.appendChild(saveEl);
 
+    saveEl.addEventListener("click", handleScore);
 
-function handleScore(event) {
-  event.preventDefault();
-  const playerName = inputEl.value;
-  const scoresList = document.createElement("ol");
-  score = score.toString();
+    // here, here, here, inspect these elements for typos
 
-  if (!localStorage.getItem("high-scores")) {
-    localStorage.setItem("high-scores", score);
-    localStorage.setItem("name", playerName);
-  }
+    function handleScore(event) {
+      console.log("handlescore");
+      event.preventDefault();
+      const playerName = inputEl.value;
+      const scoresList = document.createElement("ol");
+      score = score.toString();
 
-  let highScores = localStorage.getItem("high-scores");
-  highScores = highScores.split(",");
-  let names = localStorage.getItem("name");
-  names = names.split(",");
-  containerEl.append(scoresList);
-  scoresList.textContent = "Top Scores: ";
-  scoresList.style.fontSize = "35px";
+      if (!localStorage.getItem("high-scores")) {
+        localStorage.setItem("high-scores", score);
+        localStorage.setItem("name", playerName);
+      }
 
-  handleHighScorePlacement();
-}
+      let highScores = localStorage.getItem("high-scores");
+      highScores = highScores.split(",");
+      let names = localStorage.getItem("name");
+      names = names.split(",");
+      containerEl.append(scoresList);
+      scoresList.textContent = "Top Scores: ";
+      scoresList.style.fontSize = "35px";
 
-function handleHighScorePlacement() {
-  const scoresCount = highScores.length;
-  let placeholder = 0;
-  for (i = 0; i < scoresCount; i++) {
-    if (
-      parseInt(score) > parseInt(highScores[i]) &&
-      highScores[i] !== placeholder
-    ) {
-      placeholder = highScores[i];
-      console.log(placeholder);
-      highScores.splice(i, 0, score);
-      names.splice(i, 0, playerName);
+      handleHighScorePlacement();
+
+      function handleHighScorePlacement() {
+        console.log("highscore place");
+        const scoresCount = highScores.length;
+        let placeholder = 0;
+        for (i = 0; i < scoresCount; i++) {
+          if (
+            parseInt(score) > parseInt(highScores[i]) &&
+            highScores[i] !== placeholder
+          ) {
+            placeholder = highScores[i];
+            console.log(placeholder);
+            highScores.splice(i, 0, score);
+            names.splice(i, 0, playerName);
+          }
+          if (highScores.length < 3 && score !== highScores[i]) {
+            highScores.push(score);
+            names.push(playerName);
+          }
+        }
+
+        while (highScores.length > 3) {
+          highScores.pop();
+          names.pop();
+        }
+
+        for (let i = 0; i < highScores.length; i++) {
+          const listEl = document.createElement("li");
+          listEl.textContent = `${names[i]}      ......................      Score: ${highScores[i]}`;
+          listEl.classList.add("score-li");
+          scoresList.appendChild(listEl);
+        }
+
+        highScores = highScores.toString();
+        names = names.toString();
+        localStorage.setItem("high-scores", highScores);
+        localStorage.setItem("name", names);
+        document.querySelector("#initials-input").remove();
+        event.target.remove();
+      }
+      createEndGameBtn();
+
+      function createEndGameBtn() {
+        console.log("create endgame button");
+        const clearStorageBtn = document.createElement("btn");
+        clearStorageBtn.id = "clear-storage";
+        clearStorageBtn.classList.add("danger-btn");
+        clearStorageBtn.textContent = "Clear Scores";
+        clearStorageBtn.addEventListener("click", handleClearStorage);
+
+        const retryBtn = document.createElement("btn");
+        retryBtn.id = "retry";
+        retryBtn.classList.add("danger-btn");
+        retryBtn.textContent = "Retry";
+        retryBtn.addEventListener("click", handleRetry);
+
+        document.body.appendChild(retryBtn);
+        document.body.appendChild(clearStorageBtn);
+
+        function handleClearStorage(event) {
+          localStorage.clear();
+          scoresList.remove();
+        }
+
+        function handleRetry(event) {
+          location.reload();
+        }
+      }
     }
-    if (highScores.length < 3 && score !== highScores[i]) {
-      highScores.push(score);
-      names.push(playerName);
-    }
-  }
-
-  while (highScores.length > 3) {
-    highScores.pop();
-    names.pop();
-  }
-
-  for (let i = 0; i < highScores.length; i++) {
-    const listEl = document.createElement("li");
-    listEl.textContent = `${names[i]}      ......................      Score: ${highScores[i]}`;
-    listEl.classList.add("score-li");
-    scoresList.appendChild(listEl);
-  }
-
-  highScores = highScores.toString();
-  names = names.toString();
-  localStorage.setItem("high-scores", highScores);
-  localStorage.setItem("name", names);
-  document.querySelector("#initials-input").remove();
-  event.target.remove();
-
-  makeEndgameButtons();
-}
-
-function createEndGameBtn() {
-  const clearStorageBtn = document.createElement("btn");
-  clearStorageBtn.id = "clear-storage";
-  clearStorageBtn.classList.add("danger-btn");
-  clearStorageBtn.textContent = "Clear Scores";
-  clearStorageBtn.addEventListener("click", handleClearStorage);
-
-  const retryBtn = document.createElement("btn");
-  retryBtn.id = "retry";
-  retryBtn.classList.add("danger-btn");
-  retryBtn.textContent = "Retry";
-  retryBtn.addEventListener("click", handleRetry);
-
-  document.body.appendChild(retryBtn);
-  document.body.appendChild(clearStorageBtn);
-
-  function handleClearStorage(event) {
-    localStorage.clear();
-    scoresList.remove();
-  }
-
-  function handleRetry(event) {
-    location.reload();
   }
 }
-
 startBtnEl.onclick = beginStartButton;
