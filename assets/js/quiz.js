6337c9bc3996ed19f4c5cf22a5b9ab47fc72e648
Zachary Clark <zacclark369@gmail.com>
2021-12-05 16:38:22 -0800

    added more styling and readme

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,61 +1,51 @@
 const questionsArray = [
   {
-    Question: "what is x",
-    options: ["x", "y", "z", "p"],
-    correctAnswer: "x",
+    Question: "How many fundamental data types are ther in Javascript?",
+    options: ["7", "12", "3", "5"],
+    correctAnswer: "7",
   },
   {
-    Question: "why",
-    options: ["why not", "ad", "f", "z"],
-    correctAnswer: "z",
+    Question: "What property of strings returns the amount of characters in the string?",
+    options: ["length", "state", "string", "char"],
+    correctAnswer: "length",
   },
   {
-    Question: "kadf",
-    options: ["ad", "fa", "da", "dada"],
-    correctAnswer: "dada",
+    Question: "What method of strings returns a boolean value for whether or not the string begins with a given letter?",
+    options: ["capitalLetter", "startsWith()", "append", "textContent"],
+    correctAnswer: "startsWith()",
   },
-  //   {
-  //     Question: "",
-  //     options: ["", "", "", ""],
-  //     correctAnswer: "",
-  //   },
-  //   {
-  //     Question: "",
-  //     options: ["", "", "", ""],
-  //     correctAnswer: "",
-  //   },
-  //   {
-  //     Question: "",
-  //     options: ["", "", "", ""],
-  //     correctAnswer: "",
-  //   },
-  //   {
-  //     Question: "",
-  //     options: ["", "", "", ""],
-  //     correctAnswer: "",
-  //   },
-  //   {
-  //     Question: "",
-  //     options: ["", "", "", ""],
-  //     correctAnswer: "",
-  //   },
+    {
+      Question: "What Number method returns a boolean value based on whether or not a given number is an integer?",
+      options: ["[i]", "xIs", "isInteger(number)", "numberValue"],
+      correctAnswer: "isInteger(number)",
+    },
+    {
+      Question: "What method of strings transforms the text to uppercase?",
+      options: ["capitalLetter", "startsWith()", "append", "textContent", "toUpperCase"],
+      correctAnswer: "toUpperCase",
+    },
+    {
+      Question: "What Math method returns a random decimal value between 0 and 1?",
+      options: ["random", "randomDecimal", "math", "mathFloor"],
+      correctAnswer: "random",
+    },
+    {
+      Question: "What method of strings deletes all whitespace at the beginning and end of the string?",
+      options: ["removeWhite", "blank()", "trim()", "remove()"],
+      correctAnswer: "trim()",
+    },
+    {
+      Question: "How are single line comments formatted?",
+      options: ["//", "?", "<-- -->", "(//)"],
+      correctAnswer: "//",
+    },
 ];
 
-// begin with timer of 1:30 min (90sec)
-// user will click begin/start button
-// first question will be displayed
-// user selects answer
-// message (alert) display correct or incorrect
-// display correct if correct answer
-// display incorrect if incorrect answer
-// check for more questions
-// if so move to next question
-// if done then display score
-// store score in localStorage
-// show form to store name with score
-// display 'conrats! your score is: '
-// display the top 3 scores
+// if get extra time, seperate into different formulas and then one formula calls them all.
+// this will be easier to keep track of
 
+
+// begin with timer of 1:30 min (90sec)
 let timeRemaining = 90;
 const countdownEl = document.getElementById("countdown");
 // countdownEl.textContent = ''
@@ -71,6 +61,7 @@ const announceEl = document.getElementById("announcement");
 const containerEl = document.getElementById("question-container");
 const listItems = document.getElementsByTagName("li");
 
+// user will click begin/start button
 function beginStartButton() {
   startCountdown();
   startBtnEl.remove();
@@ -90,7 +81,7 @@ function beginStartButton() {
   }
   startQuestions();
 }
-
+// first question will be displayed
 function startQuestions() {
   if (questionsArray[questionCount]) {
     questionTitleEl.textContent = questionsArray[questionCount].Question;
@@ -105,7 +96,9 @@ function startQuestions() {
     }
   }
 }
+// user selects answer
 function questionClick() {
+  // check for more questions
   if (questionCount === questionsArray.length) {
     endGame(timeRemaining);
   }
@@ -114,15 +107,18 @@ function questionClick() {
   console.log(usersChoice);
   const correctAnswer = questionsArray[questionCount].correctAnswer;
   questionCount++;
+  // message (alert) display correct or incorrect
   if (usersChoice === correctAnswer) {
     textColorEl.textContent = "Correct Answer! ";
     textColorEl.style.color = "green";
     announceEl.textcontent = "Next Question:";
+    // display correct if correct answer
   } else {
     timeRemaining -= 5;
     textColorEl.textContent = "Wrong Answer! ";
     textColorEl.style.color = "red";
     announceEl.textcontent = "-5 seconds, please repeat the question: ";
+    // display incorrect if incorrect answer
   }
   startQuestions();
 }
@@ -134,7 +130,7 @@ function endGame(timeRemaining) {
   if (score < 0) {
     score = 0;
   }
-
+// if done then display score
   countdownEl.remove();
   textColorEl.textContent = "Congrats, you've completed the Quiz";
   textColorEl.style.color = "Black";
@@ -143,7 +139,7 @@ function endGame(timeRemaining) {
   questionEl.remove();
 
   scoreForm();
-
+// show form to store name with score
   function scoreForm() {
     console.log("scoreForm");
     const inputEl = document.createElement("INPUT");
@@ -165,7 +161,7 @@ function endGame(timeRemaining) {
     saveEl.addEventListener("click", handleScore);
 
     // here, here, here, inspect these elements for typos
-
+// store score in localStorage
     function handleScore(event) {
       console.log("handlescore");
       event.preventDefault();
@@ -213,6 +209,8 @@ function endGame(timeRemaining) {
           names.pop();
         }
 
+
+        // display the top 3 scores
         for (let i = 0; i < highScores.length; i++) {
           const listEl = document.createElement("li");
           listEl.textContent = `${names[i]}      ......      Score: ${highScores[i]}`;
@@ -233,13 +231,13 @@ function endGame(timeRemaining) {
         console.log("create endgame button");
         const clearStorageBtn = document.createElement("btn");
         clearStorageBtn.id = "clear-storage";
-        clearStorageBtn.classList.add("danger-btn");
+        clearStorageBtn.classList.add("clear-btn");
         clearStorageBtn.textContent = "Clear Scores";
         clearStorageBtn.addEventListener("click", handleClearStorage);
 
         const retryBtn = document.createElement("btn");
         retryBtn.id = "retry";
-        retryBtn.classList.add("danger-btn");
+        retryBtn.classList.add("clear-btn");
         retryBtn.textContent = "Retry";
         retryBtn.addEventListener("click", handleRetry);
 
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Javascript Fundamentals Quiz
+
+## Purpose
+A website that holds a quiz for javascript fundamentals. Starts with a timer and
+ work through a series of questions, presenting correct or incorrect answers. 
+ Finishes with scoreboard using local storage.
+
+
+## Built with
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://zaclark369.github.io/JavaScript-quiz-fundamentals/
+
+## Screenshot
+The websites landing page.
+   ![screenshot of the website](Assets/Images/screencapture.png)
+
+
+## Contribution
+Made by Zachary Clark
+* https://github.com/zaclark369
+
+### ©️2021 
